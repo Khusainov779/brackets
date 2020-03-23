b271f2479d722e580b693f52783da44b01f3b9af
Khusainov779 <khusainov779@gmail.com>
2020-03-23 16:09:13 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let brackets = bracketsConfig.map(index => index.join(""));
+    let inputs = str.length / 2;
+
+    for (let i = 0; i < inputs; i++) {
+        for (let result of brackets) {
+            str = str.replace(result, "");
+        }
+    }
+    return !str;
+};
